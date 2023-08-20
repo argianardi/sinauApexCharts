f053b6b85cf514d9be84e5a7a28b6f98e11309e7
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-08-20 22:31:50 +0700

    Feat: Add new pages

---
+++ src/pages/OpenChart1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OpenChart1 = () => {
+  return <div>OpenChart1</div>;
+};
+
+export default OpenChart1;
---
+++ src/pages/OpenChart2.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OpenChart2 = () => {
+  return <div>OpenChart2</div>;
+};
+
+export default OpenChart2;
